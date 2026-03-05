4f3bf6dad7380f6fc645d976a037062374d227e3
Raphael Lancel <lancel.raphaelalexandre@gmail.com>
2026-03-05 12:45:49 +0100

    add test file

---
+++ TP3/ex1_1/src/fonction.c
@@ -0,0 +1,7 @@
+
+#include <stdio.h>
+
+void fonction_test(void)
+{
+    printf("Test pour multi exo\n");
+}
---
+++ TP3/ex1_1/src/main.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+void fonction_test(void);
+
+int main(void)
+{
+    printf("test du main\n");
+    fonction_test();
+    return 0;
+}
